e41cc7063211d3ec820221edb318ce155bad06a6
Chad C Williams <ccwillia@uvic.ca>
2023-02-20 09:07:59 -0800

    Converted script into functions

--- autoRA_webScrapingSearch.py
+++ autoRA_webScrapingSearch.py
@@ -69,6 +69,58 @@ def removeLinks(listOfInterest):
     keptLinks = [x for x in pageLinks if '#cite' not in x] #Remove unwanted #cite links
     return keptLinks
 
+def extractLinks(links, saveName):
+    numberEquations = 0 #Initiate equation count
+    fullInitial = time.time() #Initiate timing for entire process
+    segmentInitial = time.time() #Initiate timing for each segment
+    for linkIndex, link in enumerate(links):
+        #First, add page links to link list
+        if link: #Only run if link exists
+            
+            #Display metrics for every 10 links scraped
+            if linkIndex%10 == 0:
+                print('Current Link: ' + 'https://en.wikipedia.org/' + link)
+                print('Current Link Index: ' + str(linkIndex))
+                print('Total Number of Links: ' + str(len(links)))
+                print('Current Percentage Complete: ' + str(round((linkIndex/len(links)*100),4)) + '%')
+            
+            #Download webpage
+            linkText = requests.get('https://en.wikipedia.org/' + link).text
+            
+            #Create Soup Object
+            categorySoup = BeautifulSoup(linkText, 'lxml', parse_only = SoupStrainer("div",{"id":"mw-normal-catlinks"}))
+            
+            #Scan all equations on this page
+            equations = []
+            
+            #Begin by searching for math tags
+            mathTag = 1
+            equationSoup = BeautifulSoup(linkText, 'lxml', parse_only = SoupStrainer('math',{'xmlns':'http://www.w3.org/1998/Math/MathML'}))
+            equations = equationSoup.find_all('math',{'xmlns':'http://www.w3.org/1998/Math/MathML'})
+
+            #Save equations to a file
+            if equations: #If equations exist on this page
+                with open('Data/'+saveName, 'a') as f: #Open file to be saved to
+                    titleSoup = BeautifulSoup(linkText, 'lxml', parse_only = SoupStrainer('h1')) #Extract title of page
+                    root = titleSoup.find('h1').text #Convert title to be saved
+                    _ = f.write('#ROOT: ' + root + '\n') #Save title to file
+                    _ = f.write('#LINK: '+ link + '\n') #Save link to file
+                    if mathTag:
+                        _ = [f.write(currentEquation['alttext'] + '\n') for currentEquation in equations] #Save all equations to file
+                    else:
+                        _ = [f.write(currentEquation['alt'] + '\n') for currentEquation in equations] #Save all equations to file
+                    _ = f.write('#--------------------#\n\n') #Add separator to file
+                numberEquations += len(equations) #Increase equation count for metric report
+                    
+            #Display metrics for every 10 links scraped 
+            if linkIndex%10 == 0: 
+                print('Total Equations Added: ' + str(numberEquations))
+                print('Time Since Last Report: ' + str(round(time.time()-segmentInitial,2)) + ' seconds')
+                print('----------------------------------------')
+                segmentInitial = time.time() #Reset timer
+            
+    print('Web Scraping Complete in ' +str(round(time.time()-fullInitial,2)) + ' seconds!')
+    
 ###############################################################################
 #3. Determine all links to be scraped
 ###############################################################################
@@ -89,53 +141,4 @@ links = removeLinks(expandedLinks)
 #4. Extract and Save Equations from Each Link
 ###############################################################################
 
-numberEquations = 0 #Initiate equation count
-fullInitial = time.time() #Initiate timing for entire process
-segmentInitial = time.time() #Initiate timing for each segment
-for linkIndex, link in enumerate(links):
-    #First, add page links to link list
-    if link: #Only run if link exists
-        
-        #Display metrics for every 10 links scraped
-        if linkIndex%10 == 0:
-            print('Current Link: ' + 'https://en.wikipedia.org/' + link)
-            print('Current Link Index: ' + str(linkIndex))
-            print('Total Number of Links: ' + str(len(links)))
-            print('Current Percentage Complete: ' + str(round((linkIndex/len(links)*100),4)) + '%')
-        
-        #Download webpage
-        linkText = requests.get('https://en.wikipedia.org/' + link).text
-        
-        #Create Soup Object
-        categorySoup = BeautifulSoup(linkText, 'lxml', parse_only = SoupStrainer("div",{"id":"mw-normal-catlinks"}))
-        
-        #Scan all equations on this page
-        equations = []
-        
-        #Begin by searching for math tags
-        mathTag = 1
-        equationSoup = BeautifulSoup(linkText, 'lxml', parse_only = SoupStrainer('math',{'xmlns':'http://www.w3.org/1998/Math/MathML'}))
-        equations = equationSoup.find_all('math',{'xmlns':'http://www.w3.org/1998/Math/MathML'})
-
-        #Save equations to a file
-        if equations: #If equations exist on this page
-            with open('Data/'+saveName, 'a') as f: #Open file to be saved to
-                titleSoup = BeautifulSoup(linkText, 'lxml', parse_only = SoupStrainer('h1')) #Extract title of page
-                root = titleSoup.find('h1').text #Convert title to be saved
-                _ = f.write('#ROOT: ' + root + '\n') #Save title to file
-                _ = f.write('#LINK: '+ link + '\n') #Save link to file
-                if mathTag:
-                    _ = [f.write(currentEquation['alttext'] + '\n') for currentEquation in equations] #Save all equations to file
-                else:
-                    _ = [f.write(currentEquation['alt'] + '\n') for currentEquation in equations] #Save all equations to file
-                _ = f.write('#--------------------#\n\n') #Add separator to file
-            numberEquations += len(equations) #Increase equation count for metric report
-                
-        #Display metrics for every 10 links scraped 
-        if linkIndex%10 == 0: 
-            print('Total Equations Added: ' + str(numberEquations))
-            print('Time Since Last Report: ' + str(round(time.time()-segmentInitial,2)) + ' seconds')
-            print('----------------------------------------')
-            segmentInitial = time.time() #Reset timer
-            
-print('Web Scraping Complete in ' +str(round(time.time()-fullInitial,2)) + ' seconds!')
+extractLinks(links, saveName)
